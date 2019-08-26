7a181300c5961395d22068da30cc3f4788b115af
yuyi <962919526@qq.com>
2019-08-26 11:03:20 +0800

    modify the nodes

--- dnn.py
+++ dnn.py
@@ -94,17 +94,17 @@ def dnn_interface(input_tensor, output_shape, regularizer_rate=None, drop=None):
 
 def keras_dnn_interface(input_tensor, output_shape, regularizer_rate=None, drop=None):
     
-    layer1 = keras.layers.Dense(128, activation='tanh', kernel_regularizer=regularizer_rate)(input_tensor)
+    layer1 = keras.layers.Dense(LAYER1_NODE, activation='tanh', kernel_regularizer=regularizer_rate)(input_tensor)
 
     if drop is not None:
         layer1 = keras.layers.Dropout(drop)(layer1)
     
-    layer2 = keras.layers.Dense(256, activation='tanh', kernel_regularizer=regularizer_rate)(layer1)
+    layer2 = keras.layers.Dense(LAYER2_NODE, activation='tanh', kernel_regularizer=regularizer_rate)(layer1)
     
     if drop is not None:
         layer2 = keras.layers.Dropout(drop)(layer2)
     
-    layer3 = keras.layers.Dense(128, activation='tanh', kernel_regularizer=regularizer_rate)(layer2)
+    layer3 = keras.layers.Dense(LAYER3_NODE, activation='tanh', kernel_regularizer=regularizer_rate)(layer2)
     
     if drop is not None:
         layer3 = keras.layers.Dropout(drop)(layer3)
